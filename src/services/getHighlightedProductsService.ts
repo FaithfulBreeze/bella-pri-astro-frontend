e8c05337ef17d85ff574e9ebb1c05bf09ab3afc2
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-10-11 11:53:34 -0300

    feat: .env

--- src/services/getHighlightedProductsService.ts
+++ src/services/getHighlightedProductsService.ts
@@ -7,7 +7,7 @@ interface ResponseProps {
 
 export class GetHighlightedProductsService {
   static async execute() {
-    const response = await fetch("http://localhost:3000/products/highlighted");
+    const response = await fetch(`${process.env.BACKEND_URL}/products/highlighted`);
     const data: ResponseProps = await response.json();
     return data.products;
   }
