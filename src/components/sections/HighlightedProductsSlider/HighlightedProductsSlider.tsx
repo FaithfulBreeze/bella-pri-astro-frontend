90e7537e67e96f02cc4bac7c86f139e50a2ca4fa
FaithfulBreeze <faithfulbreeze@outlook.com>
2025-10-20 21:51:27 -0300

    chore: rename default swiper config

--- src/components/sections/HighlightedProductsSlider/HighlightedProductsSlider.tsx
+++ src/components/sections/HighlightedProductsSlider/HighlightedProductsSlider.tsx
@@ -3,11 +3,15 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import ProductCard from "../../elements/ProductCard/ProductCard";
-import { swiperConfig } from "../../../common/config/swiper";
+import { defaultSwiperConfig } from "../../../common/config/swiper";
 import type { IProduct } from "../../../interfaces/product";
 import { GetHighlightedProductsService } from "../../../services/getHighlightedProductsService";
 
-export default function HighlightedProductsSlider() {
+export default function HighlightedProductsSlider({
+  classname,
+}: {
+  classname?: string;
+}) {
   const [products, setProducts] = useState<IProduct[]>([]);
 
   useEffect(() => {
@@ -17,8 +21,8 @@ export default function HighlightedProductsSlider() {
   }, []);
 
   return (
-    <div id="highlights" className="block mx-auto max-w-[1480px] px-7">
-      <Swiper {...swiperConfig}>
+    <div id="highlights" className={`block mx-auto max-w-[1480px] px-7 ${classname}`}>
+      <Swiper {...defaultSwiperConfig}>
         {products.map((product) => (
           <SwiperSlide key={product.id} className="overflow-hidden">
             <ProductCard {...product} isSliderCard={true} />
